b3510980bc54035b8470c21c11decb9a5e306d2b
William Stolworthy <willstolworthy@proton.me>
2026-07-27 09:18:17 +0100

    completed most of the sidebar

--- js/main.js
+++ js/main.js
@@ -11,4 +11,14 @@ $(document).ready(function(){
         autoplaySpeed: 6000,
         swipeToSlide: true,
     });
+
+    // sidebar toggle
+    $('.hamburger').on('click', function(){
+        $('body').toggleClass('sidebar-open');
+    });
+
+    // close sidebar when clicking the overlay (anywhere off the sidebar)
+    $('.sidebar-overlay').on('click', function(){
+        $('body').removeClass('sidebar-open');
+    });
 });
