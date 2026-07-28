c4226c780f0203a66c2d62104a68971a21f327df
William Stolworthy <willstolworthy@proton.me>
2026-07-28 09:43:34 +0100

    added sticky header

--- js/main.js
+++ js/main.js
@@ -17,8 +17,29 @@ $(document).ready(function(){
         $('body').toggleClass('sidebar-open');
     });
 
-    // close sidebar when clicking the overlay (anywhere off the sidebar)
+    // close sidebar when clicking the overlay
     $('.sidebar-overlay').on('click', function(){
         $('body').removeClass('sidebar-open');
+    });
+
+    // sticky header
+    var $header = $('.header');
+    var lastScroll = window.scrollY;
+
+    $(window).on('scroll', function(){
+        var current = window.scrollY;
+        var delta = current - lastScroll;
+
+        // ignore small scroll
+        if (Math.abs(delta) < 5) return;
+
+        // only hide once past the header
+        if (delta > 0 && current > $header.outerHeight() && !$('body').hasClass('sidebar-open')) {
+            $header.addClass('header-hidden');
+        } else {
+            $header.removeClass('header-hidden');
+        }
+
+        lastScroll = current;
     });
 });
